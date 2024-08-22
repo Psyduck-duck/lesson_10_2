ffde2b7b173d2f55edc4cf99f574251c6a7cce06
Anton Fomenko <antonfomenko1234@gmail.com>
2024-08-22 10:43:00 +0300

    some fixes

--- tests/test_task2.py
+++ tests/test_task2.py
@@ -2,20 +2,46 @@ import pytest
 from src.task2 import calculate_tax
 
 
-@pytest.mark.parametrize('price, tax_rate, expected', [(100, 10, 110),
-                                                      (200, 30, 260),
-                                                      (300, 10, 330),
-                                                      (100, 20, 120)])
-
+@pytest.mark.parametrize(
+    "price, tax_rate, expected",
+    [(100, 10, 110), (200, 30, 260), (300, 10, 330), (100, 20, 120)],
+)
 def test_calculate_tax(price, tax_rate, expected):
     calculate_tax(price, tax_rate) == expected
 
 
-def test_calculate_invalid_tax():
+@pytest.mark.parametrize(
+    "price, tax_rate, discount, expected",
+    [(100, 10, 10, 99), (200, 30, 50, 130), (300, 10, 0, 330), (100, 20, 100, 0)],
+)
+def test_calculate_tax_discount(price, tax_rate, discount, expected):
+    calculate_tax(price, tax_rate, discount) == expected
+
+
+def test_calculate_tax_discount_under_zero():
+    with pytest.raises(ValueError):
+        calculate_tax(100, 10, -10)
+
+
+def test_caclcutate_tax_discount_after_100():
+    with pytest.raises(ValueError):
+        calculate_tax(100, 10, 101)
+
+
+def test_calculate_tax_invalid_tax():
     with pytest.raises(ValueError):
         calculate_tax(100, -1)
 
 
-def test_calculate_invalid_price():
+def test_calculate_tax_invalid_price():
     with pytest.raises(ValueError):
         calculate_tax(0, 10)
+
+
+def test_calculate_tax_round():
+    assert calculate_tax(123, 10, 15, 0) == 115.00
+
+
+def test_calculate_tax_incorrect_arguments():
+    with pytest.raises(TypeError):
+        calculate_tax('123', 2)
