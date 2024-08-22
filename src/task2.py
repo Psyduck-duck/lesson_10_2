5cfefd4f1651ac2a474a8f315c2e0f14929271c3
Anton Fomenko <antonfomenko1234@gmail.com>
2024-08-22 10:42:42 +0300

    fixes calculate_tax

--- src/task2.py
+++ src/task2.py
@@ -1,8 +1,17 @@
-def calculate_tax(price: float, tax_rate: float) -> float:
+def calculate_tax(price: float, tax_rate: float, discount: float = 0, symbol_round: int = 2) -> float:
+
     if price <= 0:
         raise ValueError("Отрицательная стоимость")
-    elif tax_rate < 0:
-        raise ValueError('Отрицательная налог')
+
+    if tax_rate < 0 or tax_rate >= 100:
+        raise ValueError('Некорректный налог')
+
+    if discount < 0 or discount > 100:
+        raise ValueError('Некорректная скидка')
+
     tax_price = price * tax_rate / 100
     full_price = price + tax_price
-    return full_price
+    discount_price = full_price * discount /100
+    total_price = round(full_price - discount_price, symbol_round)
+
+    return total_price
